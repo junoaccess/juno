28fd210eebe28163472589fc4ced33bbff3e2c7b
Jerome Thayananthajothy <tjthavarshan@gmail.com>
2025-12-01 11:31:14 +0530

    Refactor import paths for Profile and Password controllers to use the Web directory

--- resources/js/pages/settings/profile.tsx
+++ resources/js/pages/settings/profile.tsx
@@ -1,4 +1,4 @@
-import ProfileController from '@/actions/App/Http/Controllers/Settings/ProfileController';
+import ProfileController from '@/actions/App/Http/Controllers/Web/Settings/ProfileController';
 import { send } from '@/routes/verification';
 import { type BreadcrumbItem, type SharedData } from '@/types';
 import { Transition } from '@headlessui/react';
